ec38ec991ffd5328060d5e3b4c9e936f871a021e
Uddin <Uddin@DESKTOP-CBLEUAT.fios-router.home>
2020-02-05 20:15:57 -0500

    show how git works

---
+++ GitDemo/src/main/java/com/git/GitComment.java
@@ -0,0 +1,12 @@
+package com.git;
+
+public class GitComment {
+	
+	// I created this to show how git works. 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
